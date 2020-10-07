f780d1ae837fdc628ccf9b0fc9c069db7dea142a
mutiur <mutiurrehmankhan87@gmail.com>
2020-10-07 11:29:57 +0530

    passport login

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const session = require('express-session')
+const passport = require('./passport')
+
+const app = express()
+
+app.set("view engine", "hbs")
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use(session({
+    secret: 'somesecretstring',
+    resave: true,
+    saveUninitialized: true
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use('/public', require('./routes/public'))
+app.use('/private', require('./routes/private'))
+app.use('/', require('./routes/root'))
+
+app.listen(1234, () => console.log("Server running on http://localhost:1234"))
